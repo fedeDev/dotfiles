bfb8c91e7be4c729108ab8276ebd9e4db12acf32
fedeDev <f.slashdev@gmail.com>
2018-04-25 15:02:33 +0200

    start from scratch with stow

--- bin/offlineimap.py
+++
@@ -1,8 +0,0 @@
-import os, subprocess
-def mailpasswd(acct):
-  cmd = "pass %s/email 2>/dev/null" % acct
-  pipe = subprocess.Popen(cmd, shell=True, cwd=None, env=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  (output, errout) = pipe.communicate()
-  assert not errout
-  assert pipe.returncode == 0
-  return output.strip()
--- home/.zlogout
+++
@@ -1,5 +0,0 @@
-# Cleanup gpg key file and return crontab back
-cat /dev/null > $HOME/.gnupg/lock
-cat /dev/null > $HOME/.offlineimap/log
-crontab -l > /tmp/crontab.a
-sed -e 's=^\(.*/bin/imap-cron.sh$\)=#\1=g' /tmp/crontab.a | crontab
